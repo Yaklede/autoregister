21e710695914a454782629b14ac0b3458fba99df
Yaklede <chh78151@naver.com>
2023-08-12 17:38:58 +0900

    feature: init spring project

---
+++ src/test/kotlin/com/naver/autoregister/AutoregisterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.naver.autoregister
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AutoregisterApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
