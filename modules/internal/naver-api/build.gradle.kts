afe9cd862b72265bbe76a806e3a3ce906fe1ea03
Yaklede <chh78151@naver.com>
2023-08-16 12:58:35 +0900

    feature: naver 전용 암호화 의존성 추가

--- modules/internal/naver-api/build.gradle.kts
+++ modules/internal/naver-api/build.gradle.kts
@@ -1,3 +1,5 @@
 dependencies {
     implementation(project(":modules:core:common"))
+    implementation ("org.apache.commons:commons-lang3:3.12.0")
+    implementation ("org.mindrot:jbcrypt:0.4")
 }
