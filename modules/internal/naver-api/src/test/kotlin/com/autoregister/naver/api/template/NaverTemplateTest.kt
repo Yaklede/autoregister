4b471e9beacc291c34bc63467635ed30130ad32e
Yaklede <chh78151@naver.com>
2023-08-16 18:59:27 +0900

    test: wrong properties test추가

--- modules/internal/naver-api/src/test/kotlin/com/autoregister/naver/api/template/NaverTemplateTest.kt
+++ modules/internal/naver-api/src/test/kotlin/com/autoregister/naver/api/template/NaverTemplateTest.kt
@@ -2,8 +2,10 @@ package com.autoregister.naver.api.template
 
 import com.autoregister.naver.api.config.NaverApiAutoConfiguration
 import com.autoregister.naver.api.config.NaverApiProperties
+import com.autoregister.naver.api.template.exception.FailResponseException
 import com.autoregister.naver.api.template.request.ProductOrderIdsRequest
 import com.autoregister.naver.api.template.response.ProductOrderIdsResponse
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -16,14 +18,38 @@ import org.springframework.web.client.RestTemplate
 class NaverTemplateTest {
 
     val restTemplate = RestTemplate()
-    val properties = NaverApiProperties(
-        domain = "https://api.commerce.naver.com",
-        clientId = "yourTestId",
-        secretKey = "yourTestKey"
-    )
     @Test
-    @DisplayName("real template test")
+    @DisplayName("wrong properties")
+    fun wrongproperties() {
+        val properties = NaverApiProperties(
+            domain = "https://api.commerce.naver.com",
+            clientId = "clientId",
+            secretKey = "secretKey"
+        )
+
+        val naverTemplate = NaverTemplate(
+            properties = properties,
+            restTemplate = restTemplate
+        )
+
+        val request = ProductOrderIdsRequest(
+            orderId = "tes"
+        )
+
+        Assertions.assertThatThrownBy {
+            naverTemplate.execute(request, ProductOrderIdsResponse::class)
+        }.isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @Test
+    @DisplayName("read template")
     fun naverTemplateTest() {
+        val properties = NaverApiProperties(
+            domain = "https://api.commerce.naver.com",
+            clientId = "readId",
+            secretKey = "realKey"
+        )
+
         val naverTemplate = NaverTemplate(
             properties = properties,
             restTemplate = restTemplate
