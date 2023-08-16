ad71b8e8f82f5f55c43e529320dd0d7eee5fc651
Yaklede <chh78151@naver.com>
2023-08-16 15:11:04 +0900

    feature: 공통 request, response 인터페이스 추가

---
+++ modules/internal/naver-api/src/main/kotlin/com/autoregister/naver/api/template/common/interfaces/NaverCommerceRequest.kt
@@ -0,0 +1,11 @@
+package com.autoregister.naver.api.template.common.interfaces
+
+import com.autoregister.naver.api.template.enumaration.ServiceType
+import org.springframework.http.HttpMethod
+
+interface NaverCommerceRequest<T> {
+    val endpoint: String
+    val method: HttpMethod
+    val serviceType: ServiceType
+    val body: T?
+}
---
+++ modules/internal/naver-api/src/main/kotlin/com/autoregister/naver/api/template/common/interfaces/NaverCommerceResponse.kt
@@ -0,0 +1,7 @@
+package com.autoregister.naver.api.template.common.interfaces
+
+import com.autoregister.naver.api.template.enumaration.ServiceType
+
+interface NaverCommerceResponse {
+    val serviceType: ServiceType
+}
