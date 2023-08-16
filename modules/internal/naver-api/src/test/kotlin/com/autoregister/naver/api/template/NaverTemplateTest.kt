9e705e6dcc4f318abd8e8c331bdd6836cf80f8e0
Yaklede <chh78151@naver.com>
2023-08-16 18:48:32 +0900

    test: 실제 template호출 테스트

--- modules/internal/naver-api/src/test/kotlin/com/autoregister/naver/api/template/NaverTemplateTest.kt
+++ modules/internal/naver-api/src/test/kotlin/com/autoregister/naver/api/template/NaverTemplateTest.kt
@@ -1,18 +1,37 @@
 package com.autoregister.naver.api.template
 
 import com.autoregister.naver.api.config.NaverApiAutoConfiguration
+import com.autoregister.naver.api.config.NaverApiProperties
+import com.autoregister.naver.api.template.request.ProductOrderIdsRequest
+import com.autoregister.naver.api.template.response.ProductOrderIdsResponse
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.web.client.RestTemplate
 
-@SpringBootTest(classes = [NaverApiAutoConfiguration::class])
-class NaverTemplateTest @Autowired constructor(
-    private val naverTemplate: NaverTemplate
-) {
+
+class NaverTemplateTest {
+
+    val restTemplate = RestTemplate()
+    val properties = NaverApiProperties(
+        domain = "https://api.commerce.naver.com",
+        clientId = "yourTestId",
+        secretKey = "yourTestKey"
+    )
     @Test
-    @DisplayName("naver template test")
+    @DisplayName("real template test")
     fun naverTemplateTest() {
+        val naverTemplate = NaverTemplate(
+            properties = properties,
+            restTemplate = restTemplate
+        )
+
+        val request = ProductOrderIdsRequest(
+            orderId = "yourTesTOrderId"
+        )
+        naverTemplate.execute(request, ProductOrderIdsResponse::class)
     }
 }
