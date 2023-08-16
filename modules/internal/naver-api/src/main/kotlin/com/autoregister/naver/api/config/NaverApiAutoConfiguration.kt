a5570851ab7e878d9f6a716f7c127dc1461e0ab4
Yaklede <chh78151@naver.com>
2023-08-16 13:50:32 +0900

    feature: restTemplate bean 등록

--- modules/internal/naver-api/src/main/kotlin/com/autoregister/naver/api/config/NaverApiAutoConfiguration.kt
+++ modules/internal/naver-api/src/main/kotlin/com/autoregister/naver/api/config/NaverApiAutoConfiguration.kt
@@ -1,10 +1,24 @@
 package com.autoregister.naver.api.config
 
 import org.springframework.boot.autoconfigure.AutoConfiguration
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.web.client.RestTemplate
+import java.time.Duration
 
 @AutoConfiguration
 @ComponentScan(basePackages = ["com.autoregister.naver.api"])
 @EnableConfigurationProperties(NaverApiProperties::class)
-class NaverApiAutoConfiguration
+class NaverApiAutoConfiguration {
+    @Bean
+    @ConditionalOnBean(RestTemplate::class)
+    fun restTemplate(): RestTemplate {
+        val restTemplateBuilder = RestTemplateBuilder()
+        restTemplateBuilder.setConnectTimeout(Duration.ofSeconds(10L))
+        restTemplateBuilder.setReadTimeout(Duration.ofSeconds(10L))
+        return restTemplateBuilder.build()
+    }
+}
