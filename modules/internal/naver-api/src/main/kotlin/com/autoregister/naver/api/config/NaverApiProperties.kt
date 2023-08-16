6c38cef6da4ab32d7c36937a7895d2f055eecbb6
Yaklede <chh78151@naver.com>
2023-08-16 11:22:24 +0900

    fix: naver properties setting 변경

--- modules/internal/naver-api/src/main/kotlin/com/autoregister/naver/api/config/NaverApiProperties.kt
+++ modules/internal/naver-api/src/main/kotlin/com/autoregister/naver/api/config/NaverApiProperties.kt
@@ -2,7 +2,6 @@ package com.autoregister.naver.api.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 
-
 @ConfigurationProperties(prefix = "naver")
 class NaverApiProperties(
     var clientId: String? = null,
