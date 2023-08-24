a52b87deea7c4af97482b3402ea4c86f01b90152
Yaklede <chh78151@naver.com>
2023-08-24 17:40:56 +0900

    feature: productinfo 클래스 추가

---
+++ modules/internal/crawling/src/main/kotlin/com/autoregister/crawling/response/domeme/ProductInfo.kt
@@ -0,0 +1,9 @@
+package com.autoregister.crawling.response.domeme
+
+data class ProductInfo(
+    val from: String,
+    val modelName: String,
+    val company:  String,
+    val volume: Double,
+    val weight: Double,
+)
